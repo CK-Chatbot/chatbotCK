46c027e8784e7e7dae6fea56e57e452200a5ec77
ankhanh <131770802+ankhanh3011@users.noreply.github.com>
2024-10-04 19:51:20 +0700

    change bucket

--- chatbot_ck/Bedrock_stack.py
+++ chatbot_ck/Bedrock_stack.py
@@ -18,25 +18,27 @@ class BedrockStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        
+
         # Create Knowledge Base
         kb = bedrock.KnowledgeBase(self, 'KnowledgeBase-OS', 
             embeddings_model= bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,
             instruction= 'Use this knowledge base to answer questions about inquiries (FaQs, services offerings,...) related to Cloud Kinetics Company',
             description= 'This knowledge base contains the information about Cloud Kinetics.',                    
         )
-        
+
         # Create S3 bucket for Bedrock data source
-        docBucket = s3.Bucket(self, 'DockBucket-OS', removal_policy=RemovalPolicy.DESTROY)
+        docBucket = s3.Bucket.from_bucket_name(
+            self,
+            "DockBucket-OS",
+            bucket_name="bedrockstack-dockbucketosabbb315d-obygcjtto2lw",
+        )
         dataSource = bedrock.S3DataSource(self, 'DataSource-OS',
             bucket= docBucket,
             knowledge_base=kb,
             data_source_name='CK-Sale-Assets',
             chunking_strategy= bedrock.ChunkingStrategy.FIXED_SIZE,
         )
-        
-        
-        
+
         CfnOutput(self, "KnowledgeBaseId", value=kb.knowledge_base_id)
         CfnOutput(self, 'DataSourceId', value= dataSource.data_source_id)
         CfnOutput(self, 'DocumentBucket', value= docBucket.bucket_name)
